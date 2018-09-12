331e52a172fab6ce528202dd3f1d2731200e76f2
alepresta <alejandro.modernizacion@gmail.com>
2018-09-12 16:06:40 -0300

    feat: Recepcion inicio

---
+++ test/cucumber/features/step_definitions/turnos/turnos_recepcionar.rb
@@ -0,0 +1,11 @@
+Then /^Generar un turno a Cardozo Campos$/ do
+  # do something
+end
+
+Then /^Recepcionar a Cardozo Campos$/ do
+  # do something
+end
+
+Then /^Atender el turno de Cardozo Campos$/ do
+  # do something
+end
