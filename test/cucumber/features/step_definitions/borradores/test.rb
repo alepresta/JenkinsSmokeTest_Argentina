5f80ee03a2743d5bdcf4c4571308a71693c47b0c
alepresta <alejandro.modernizacion@gmail.com>
2018-09-13 12:38:30 -0300

    feat: Recepcionar Turnos OK

--- test/cucumber/features/step_definitions/borradores/test.rb
+++ test/cucumber/features/step_definitions/borradores/test.rb
@@ -1,4 +1,48 @@
 Given /^en el primer paso$/ do
+  go("https://qa-usuarios-turnos.argentina.gob.ar")
+  esperar(1)
+  #captura_de_pantalla("qa-usuarios-turnos")
+  usuario = "sntagente@yopmail.com"
+  password = "QAsnt2018"
+  asd = capturar(:id,'usuario')
+  esperar(1)
+  asd.send_keys usuario
+  asd.clear()
+  asd.send_keys usuario
+  cap = capturar(:id,'password')
+  cap.send_keys password
+  esperar(1)
+  cap.clear()
+  cap.send_keys password
+  bot = capturar(:class,'btn-success')
+  bot.click
+  #############
+  titulo_seccion_inicial = "esta"
+  titulo_inicial = capturar(:class,'titulo-seccion').text
+  if titulo_inicial.include? titulo_seccion_inicial
+    salir_de_ventanilla = capturar(:xpath,'/html/body/app-root/main/div/app-turnos-cola/h1/button')
+    salir_de_ventanilla.click
+  end
+  recepcion = capturar(:xpath,'/html/body/app-root/main/div/app-ventanilla/div/div/div/div[1]/div')
+  recepcion.click
+  esperar(3)
+  cuil = "27322041328"
+  nombre = "Aaron Brian Leonel"
+  apellido = "Cardozo Campos"
+  email = "eaarached+036@gmail.com"
+
+  buscar = capturar(:id,'codigo')
+  buscar.send_keys cuil
+ boton_buscar = capturar(:xpath, '/html/body/app-root/main/div/app-turnos/div/div[2]/app-busqueda-turnos/div/form/button')
+  boton_buscar.click
+  boton_recepcionar = capturar(:xpath,'/html/body/app-root/main/div/app-ver-turno/app-edicion-turno-form/div/form/div/div[2]/div[1]/button[1]')
+  boton_recepcionar.click
+
+  guardar = capturar(:xpath,'/html/body/app-root/main/div/app-ver-turno/app-edicion-turno-form/div/form/div/div[2]/div[2]/button[2]')
+  guardar.click
+
+  volver = capturar(:xpath, '/html/body/app-root/main/div/app-ver-turno/app-edicion-turno-form/div/form/div/div[2]/div[2]/button[1]')
+  volver.click
 
 end
 
